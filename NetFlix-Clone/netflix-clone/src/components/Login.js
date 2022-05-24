c152523ffb94712bb3200e787f66dd1e497833ad
James Noleto <muricijames@gmail.com>
2022-05-24 11:51:19 -0300

    feat: styled-component login

---
+++ NetFlix-Clone/netflix-clone/src/components/Login.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Conteudo>
+        <CTA>
+          <CTALogoUm src="/imagens/cta-logo-one.svg" alt="" />
+          <Cadastro>ACESSE AGORA</Cadastro>
+          <Descricao>
+            Tenha acesso aos melhores filmes, séries e documentários.
+          </Descricao>
+          <CTALogoDois src="/imagens/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Conteudo>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Conteudo = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CTALogoUm = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const Cadastro = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Descricao = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoDois = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+export default Login;
