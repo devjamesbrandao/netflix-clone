579863ec3b92b8e4501f8c4936a710986b642961
James Noleto <muricijames@gmail.com>
2022-05-24 11:51:42 -0300

    feat: add react-router-dom

---
+++ NetFlix-Clone/netflix-clone/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
