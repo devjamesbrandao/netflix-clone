367c4bc9afae4f778170f1f8638e67de7ac10118
James Noleto <muricijames@gmail.com>
2022-05-25 18:05:25 -0300

    feat: componente Header e Home

--- NetFlix-Clone/netflix-clone/src/App.js
+++ NetFlix-Clone/netflix-clone/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Inicio from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <Header />
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/inicio" element={<Inicio />}></Route>
         </Routes>
       </Router>
     </div>
