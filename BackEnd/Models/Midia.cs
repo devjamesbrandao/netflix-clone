d37e0703aa3f4c2c886ec15275df53ac3ee979b8
James Noleto <muricijames@gmail.com>
2022-05-27 23:13:10 -0300

    feat: entidade de mídia

---
+++ BackEnd/Models/Midia.cs
@@ -0,0 +1,14 @@
+namespace BackEnd.Models
+{
+    public class Midia
+    {
+        public string Id { get; private set; }
+        public string backgroundImg { get; private set;}
+        public string cardImg { get; private set;}
+        public string description { get; private set;}
+        public string subTitle { get; private set;}
+        public string title { get; private set;}
+        public string titleImg { get; private set;}
+        public string type { get; private set;}
+    }
+}
