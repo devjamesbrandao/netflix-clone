fd939590660f4499f594b82a4cf6a20c67af53aa
James Noleto <muricijames@gmail.com>
2022-05-30 22:24:41 -0300

    refactor: styled component de recomendação

--- NetFlix-Clone/netflix-clone/src/components/Recommends.js
+++ NetFlix-Clone/netflix-clone/src/components/Recommends.js
@@ -1,29 +1,11 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { selectRecommend } from "../features/movieSlice";
+import { useSelector } from "react-redux";
 
 const Recommends = (props) => {
-  const filmes = [
-    {
-        "id": 1,
-        "backgroundImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg",
-        "cardImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/87F1DCF36049558159913ADFD18A800DE1121771540033EC3A7651B8FE154CEB/scale?width=400&aspectRatio=1.78&format=jpeg",
-        "description": "When 11-year-old Riley moves to a new city, her Emotions team up to help her through the transition. Joy, Fear, Anger, Disgust and Sadness work together, but when Joy and Sadness get lost, they must journey through unfamiliar places to get back home.",
-        "subTitle": "2015 • 1h 35m • Coming of Age, Family, Animation",
-        "title": "Inside Out",
-        "titleImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5C647DF3FFBFA343CFEA84AC715148F25F9E86F398B408010CC403E7654FB908/scale?width=1440&aspectRatio=1.78",
-        "type": "recommend"
-    },
-    {
-        "id":2,
-        "backgroundImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg",
-        "cardImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/706C68FF1CEA5A9C2AE0608892C2DF79E4AC1F0DDB4BFF7FE6DAFC36DAFC0286/scale?width=400&aspectRatio=1.78&format=jpeg",
-        "description": "A Chinese mom who’s sad when her grown son leaves home gets another chance at motherhood when one of her dumplings springs to life. But she finds that nothing stays cute and small forever.",
-        "subTitle": "2018 • 7m • Family, Fantasy, Kids, Animation",
-        "title": "Bao",
-        "titleImg": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78",
-        "type": "recommend"
-    }
-  ];
+  
+  const filmes = useSelector(selectRecommend);
 
   return (
     <Container>
